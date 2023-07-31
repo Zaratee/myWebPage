ce8bcbdc8b0cd7a929b6fc9183b859f1413b1aa7
Zaratee <zarate251098@hotmail.com>
2023-07-30 18:15:11 -0600

    my pallette

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'android-logo': "url('../public/androidLogo.png')",
+        'java-logo': "url('../public/java-logo.png')",
+      },
+      colors: {
+        'yo-color-1': '#931621',
+        'yo-color-2': '#010400',
+        'yo-color-3': '#30332E',
+        'yo-color-4': '#FFFBFC',
+        'yo-color-5': '#62BBC1',
+      }
+    },
+
   },
   plugins: [],
 }
